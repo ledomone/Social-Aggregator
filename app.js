dc490aa296e6598a3a6b3b2aafbb37c76c2354fd
ledomone <ledom.git@gmail.com>
2016-11-26 13:39:45 +0100

    Setting up Google credentials

--- app.js
+++ app.js
@@ -14,13 +14,14 @@ var app = express();
 var GoogleStrategy = require('passport-google-oauth').OAuth2Strategy;
 
 passport.use(new GoogleStrategy({
-  clientId: '',
-  clientSecret: '',
-  callbackURL: '',
+    clientID: '350049390568-ajcnkficbsq7e81sm7rjk5k6r8k28g7n.apps.googleusercontent.com',
+    clientSecret: 'cjzJDr5-1XZOFFRV5VydQ2il',
+    callbackURL: 'http://localhost:3000/auth/google/callback'
+  },
   function(req, accessToken, refreshToken, profile, done) {
     done(null, profile);
   }
-}));
+));
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
