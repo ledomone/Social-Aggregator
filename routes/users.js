0365b7d9ef32b6b7a058c1d379f6281a478796cb
ledomone <ledom.git@gmail.com>
2016-11-26 18:08:52 +0100

    Securing Routes

--- routes/users.js
+++ routes/users.js
@@ -1,6 +1,13 @@
 var express = require('express');
 var router = express.Router();
 
+router.use('/', function(req, res, next) {
+  if (!req.user) {
+    // user not logged in -> redirect
+    res.redirect('/');
+  }
+  next();
+});
 /* GET users listing. */
 router.get('/', function(req, res, next) {
   res.render('users', {user: {name: req.user.displayName,
